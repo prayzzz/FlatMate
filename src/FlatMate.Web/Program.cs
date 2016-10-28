78ea2a8854a7d2dbcd25ff222810e80fa734a5d0
prayzzz <prayzzz@outlook.com>
2016-10-28 18:41:31 +0200

    - changed listening urls

--- src/FlatMate.Web/Program.cs
+++ src/FlatMate.Web/Program.cs
@@ -11,7 +11,7 @@ namespace FlatMate.Web
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:5000", "http://192.168.1.27:5000")
+                .UseUrls("http://localhost:5000")
                 .Build();
 
             host.Run();
